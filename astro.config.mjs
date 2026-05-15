ad9d7017cff9109b5c44ac3ff20b31ec22f89be7
Leonardo Rivera <77716335+dev-leonardorivera@users.noreply.github.com>
2026-05-15 23:49:57 +0200

    Configure GitHub Pages and update integrations
    Updated GitHub Pages configuration and integrated astro-icon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
- import icon from "astro-icon";
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon({
+  // 1. Añadimos la URL de tu GitHub Pages y la subcarpeta del repositorio
+  site: 'https://dev-leonardorivera.github.io',
+  base: '/digital-menu-astro',
+  trailingSlash: 'always', // Recomendado para evitar problemas de rutas en GitHub Pages
+
+  integrations: [
+    icon({
       iconSets: {
         mdi: () => import('@iconify-json/mdi/icons.json'),
       },
-    })],
+    })
+  ],
   vite: {
-    plugins: [tailwindcss()
-    ]
+    plugins: [tailwindcss()]
   }
 });
