07eaa9b69b13e90e77d58b2983085f2bec36f6a1
dev-leonardorivera <leonardorivera1004@gmail.com>
2026-05-16 00:48:45 +0200

    fix router pages v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
+  // 1. Configuramos las rutas para GitHub Pages
+  site: "https://dev-leonardorivera.github.io",
+  base: "/digital-menu-astro",
+  trailingSlash: "always", // Obliga a Astro a usar '/' al final de las rutas, ideal para GitHub Pages
+
   integrations: [
     icon({
       iconSets: {
